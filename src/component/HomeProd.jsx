2d56a6060dc3ab4832cf19d433c5bd075f1f6cdb
rahulrawat122122 <rahulrawat122122@gmail.com>
2023-01-19 14:38:58 +0530

    Done

--- src/component/HomeProd.jsx
+++ src/component/HomeProd.jsx
@@ -118,7 +118,7 @@ const HomeProd = () => {
                 />
                 <div className="card-body">
                   <h5 className="card-title mb-0">
-                    {product.title.slice(0, 150)}...
+                    {product.title.slice(0, 12)}...
                   </h5>
                   <p className="card-text lead fw-bold">$ {product.price}</p>
                   <NavLink
